44f011e8a8a44cfe8caaaf8ee9f13143c7bcf146
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-07-21 07:03:27 +0530

    feat: Refactor file structure and add login and 404 pages with corresponding styles

--- Expressjs/Day4/index.js
+++ Expressjs/Day4/index.js
@@ -3,14 +3,25 @@ import path from "path";
 
 const app = express();
 
-// CSS, JS, Images serve karne ke liye
-app.use(express.static("public"));
+// Serve static files
+const publicPath = path.resolve("public");
+app.use(express.static(publicPath));
 
+// Home
 app.get("/", (req, res) => {
-    const abspath = path.resolve("html/home.html");
-    res.sendFile(abspath);
+    res.sendFile(path.resolve("html/home.html"));
+});
+
+// Login
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("html/login.html"));
+});
+
+// 404
+app.use((req, res) => {
+    res.status(404).sendFile(path.resolve("html/404.html"));
 });
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running at http://localhost:3000");
 });
